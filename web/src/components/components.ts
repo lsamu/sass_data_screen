c3fa17a81b2f251b80015855c8f99a75a776c758
lauxinyi <lauxinyi@gmail.com>
2023-09-12 17:16:17 +0800

    feat:master

--- web/src/components/components.ts
+++ web/src/components/components.ts
@@ -4,40 +4,60 @@ export default [
         icon: "",
         children: [
             {
-                id: "line",
-                name: "折线图",
+                title: "折线图",
+                componentName: "BoxEChartLine",
+                hidden: false,
+                locked: false,
             },
             {
                 id: "histogram",
                 name: "柱状图",
+                title: "柱状图",
+                componentName: "BoxEChartHistogram",
+                hidden: false,
+                locked: false,
             },
             {
-                id: "bar",
-                name: "条形图",
+                title: "条形图",
+                componentName: "BoxEChartBar",
+                hidden: false,
+                locked: false,
             },
             {
-                id: "pie",
-                name: "饼图",
+                title: "饼图",
+                componentName: "BoxEChartPie",
+                hidden: false,
+                locked: false,
             },
             {
-                id: "ring",
-                name: "环状图",
+                title: "环状图",
+                componentName: "BoxEChartRing",
+                hidden: false,
+                locked: false,
             },
             {
-                id: "funnel",
-                name: "漏斗图",
+                title: "漏斗图",
+                componentName: "BoxEChartFunnel",
+                hidden: false,
+                locked: false,
             },
             {
-                id: "radar",
-                name: "雷达图",
+                title: "雷达图",
+                componentName: "BoxEChartRadar",
+                hidden: false,
+                locked: false,
             },
             {
-                id: "map",
-                name: "中国地图",
+                title: "中国地图",
+                componentName: "BoxEChartMap",
+                hidden: false,
+                locked: false,
             },
             {
-                id: "liquidfill",
-                name: "水球图",
+                title: "水球图",
+                componentName: "BoxEChartLiquidfill",
+                hidden: false,
+                locked: false,
             },
         ]
     },
@@ -46,8 +66,34 @@ export default [
         icon: "",
         children: [
             {
-                id: "text",
-                name: "文本"
+                title: "文本",
+                componentName: "BoxEChartText",
+                hidden: false,
+                locked: false,
+            },
+            {
+                title: "时间",
+                componentName: "BoxEChartDateTime",
+                hidden: false,
+                locked: false,
+            },
+            {
+                title: "Iframe",
+                componentName: "BoxEChartIframe",
+                hidden: false,
+                locked: false,
+            },
+            {
+                title: "跑马灯",
+                componentName: "BoxEChartIframe",
+                hidden: false,
+                locked: false,
+            },
+            {
+                title: "超链接",
+                componentName: "BoxEChartIframe",
+                hidden: false,
+                locked: false,
             },
         ]
     },
@@ -56,31 +102,118 @@ export default [
         icon: "",
         children: [
             {
-                id: "image",
-                name: "图片",
+                title: "图片",
+                componentName: "BoxEChartImage",
+                hidden: false,
+                locked: false,
             },
             {
-                id: "music",
-                name: "音乐"
+                title: "图片框",
+                componentName: "BoxEChartImageBorder",
+                hidden: false,
+                locked: false,
+            },
+            {
+                title: "轮播图",
+                componentName: "BoxEChartSwiper",
+                hidden: false,
+                locked: false,
+            },
+            {
+                title: "音乐",
+                componentName: "BoxEChartMusic",
+                hidden: false,
+                locked: false,
+            },
+            {
+                title: "视频",
+                componentName: "BoxEChartVedio",
+                hidden: false,
+                locked: false,
+            },
+            {
+                title: "视频流",
+                componentName: "BoxEChartHls",
+                hidden: false,
+                locked: false,
             }
         ]
     },
     {
         title: "指标",
         icon: "",
-        children: []
+        children: [
+            {
+                title: "翻牌器",
+                componentName: "BoxEChartHls",
+                hidden: false,
+                locked: false,
+            },
+            {
+                title: "颜色块",
+                componentName: "BoxEChartHls",
+                hidden: false,
+                locked: false,
+            },
+            {
+                title: "环形图",
+                componentName: "BoxEChartHls",
+                hidden: false,
+                locked: false,
+            },
+            {
+                title: "进度条",
+                componentName: "BoxEChartHls",
+                hidden: false,
+                locked: false,
+            },
+            {
+                title: "仪表盘",
+                componentName: "BoxEChartHls",
+                hidden: false,
+                locked: false,
+            },
+            {
+                title: "字符云",
+                componentName: "BoxEChartHls",
+                hidden: false,
+                locked: false,
+            }
+        ]
     },
     {
         title: "表格",
         icon: "",
-        children: []
+        children: [
+            {
+                title: "表格",
+                componentName: "BoxEChartTable",
+                hidden: false,
+                locked: false,
+            },
+            {
+                title: "选项卡",
+                componentName: "BoxEChartTable",
+                hidden: false,
+                locked: false,
+            },
+            {
+                title: "折叠项",
+                componentName: "BoxEChartTable",
+                hidden: false,
+                locked: false,
+            },
+        ]
     },
     {
         title: "边框",
         icon: "",
         children: [
             {
-                name: "边框",
+                title: "边框",
+                componentName: "BoxEChartBorder",
+                hidden: false,
+                locked: false,
             },
         ]
     },
@@ -88,7 +221,12 @@ export default [
         title: "装饰",
         icon: "",
         children: [
-            
+            {
+                title: "装饰",
+                componentName: "BoxEChartBorder",
+                hidden: false,
+                locked: false,
+            },
         ]
     }
 ]
